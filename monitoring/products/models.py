9ac30584f90604ecad346f42d57108ea9c814069
Imrie83 <czolgista83@gmail.com>
2021-12-06 12:18:33 +0100

    add product, components and componentTools models

--- monitoring/products/models.py
+++ monitoring/products/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from tools.models import ToolsModel
 
-# Create your models here.
+
+class ComponentsModel(models.Model):
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='Component name'
+    )
+    stock = models.IntegerField(verbose_name='Stock')
+    tools_req = models.ManyToManyField(
+        to=ToolsModel,
+        through='ComponentToolsModel',
+        verbose_name='Tools Required'
+    )
+
+
+class ComponentToolsModel(models.Model):
+    component_id = models.ForeignKey(
+        ComponentsModel,
+        on_delete=models.CASCADE,
+        related_name='component'
+    )
+    tools_id = models.ForeignKey(
+        ToolsModel,
+        on_delete=models.CASCADE,
+        related_name='tool'
+    )
+    machine_time = models.IntegerField()
+
+
+class ProductsModel(models.Model):
+    job_no = models.CharField(
+        max_length=30,
+        null=False,
+        unique=True,
+        verbose_name='Job number'
+    )
+    machining_time = models.IntegerField(
+        null=True,
+        verbose_name='Total machining time'
+    )
+    frame_size = models.IntegerField(
+        null=False,
+        verbose_name='Frame total length'
+    )
+    components = models.ManyToManyField(
+        to=ComponentsModel,
+        related_name='product',
+    )
