6a2fc51deae6d0d8b8497133c08e22237a5ac21e
Imrie83 <czolgista83@gmail.com>
2021-12-06 10:16:03 +0100

    created tools model

--- monitoring/monitoring/settings.py
+++ monitoring/monitoring/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'management',
+    'products',
+    'tools',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +77,13 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "pma",
+        "USER": "postgres",
+        "PASSWORD": "coderslab",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
--- monitoring/tools/models.py
+++ monitoring/tools/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ToolsModel(models.Model):
+    tool_name = models.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='Tool name'
+    )
+    type = models.CharField(
+        max_length=255,
+        null=True,
+        verbose_name='Type'
+    )
+    stock = models.IntegerField(
+        verbose_name='In stock',
+        null=False
+    )
+    wear_limit = models.IntegerField(
+        verbose_name='Allowed use time',
+        null=True,
+    )
